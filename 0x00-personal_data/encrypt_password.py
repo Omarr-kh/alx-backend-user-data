ae52300effbfcb47c5255314f219146974efae32
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-04-13 17:47:50 +0200

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,6 +8,6 @@ def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
 
-def is_valid(hash_password: bytes, password: str) -> bool:
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """ validate the provided password matches the hashed password """
-    return bcrypt.checkpw(password.encode("utf-8"), hash_password)
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
