7b994ce6d723d4dd9ca2e5e499175db1ef861649
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-04-17 13:19:15 +0200

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -67,7 +67,16 @@ class BasicAuth(Auth):
         """Retrieves the user from a request.
         """
         auth_header = self.authorization_header(request)
+        if not auth_header:
+            return None
+
         b64_auth = self.extract_base64_authorization_header(auth_header)
+        if not b64_auth:
+            return None
+
         decoded_auth = self.decode_base64_authorization_header(b64_auth)
+        if not decoded_auth:
+            return None
+
         email, password = self.extract_user_credentials(decoded_auth)
         return self.user_object_from_credentials(email, password)
