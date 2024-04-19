474266e2be7de5449a23a43cd826ba8caa84bd72
Omarr-kh <omar1752003@gmail.com>
2024-04-19 19:22:19 +0000

    return a user based on cookie value

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,7 @@
 """ Session Auth module """
 from .auth import Auth
 from uuid import uuid4
-
+from models.user import User
 
 class SessionAuth(Auth):
     """ SessionAuth Class """
@@ -21,3 +21,8 @@ class SessionAuth(Auth):
         """ retreive session_id for a user_id """
         if type(session_id) is str:
             return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a user based on a cookie value """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
