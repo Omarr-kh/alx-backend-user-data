e54b73fd25f9215f4c18f33a565330d4e96b1884
Omarr-kh <omar1752003@gmail.com>
2024-04-19 17:46:38 +0000

    followed pycodestyle

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,6 +7,7 @@ from uuid import uuid4
 class SessionAuth(Auth):
     """ SessionAuth Class """
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """ creates a session id for a user id """
         if user_id and isinstance(user_id, str):
