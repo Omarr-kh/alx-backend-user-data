77fc17a65b008088e83fe651359db0efcceb15c2
Omarr-kh <omar1752003@gmail.com>
2024-04-20 20:28:20 +0000

    UserSession model

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" User Session Model """
+from models.base import Base
+
+
+class UserSession(Base):
+    """ User Session Class """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initializes a User session instance """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get("user_id")
+        self.session_id = kwargs.get("session_id")
