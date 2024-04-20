7068c0fc922f5102e67f1fc5bceea7fd69307e8d
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-04-20 22:01:12 +0200

    Update session_exp_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -28,4 +28,16 @@ class SessionExpAuth(SessionAuth):
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """ 
+        """ return a user id by session id """
+        if session_id in self.user_id_by_session_id:
+            session_data = self.user_id_by_session_id[session_id]
+            if self.session_duration <= 0:
+                return session_data["user_id"]
+            if not session_data.get("created_at"):
+                return None
+            time_now = datetime.now()
+            time_span = timedelta(seconds=self.session_duration)
+            expiry_time = session_dict.get('created_at') + time_span
+            if expiry_time < time_now:
+                return None
+            return session_data.get("user_id")
